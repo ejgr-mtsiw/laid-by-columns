e49ffc22a607cb0b0bc8ca22ab4e2844f896882e
Eduardo Ribeiro <1600820@estudante.uab.pt>
2022-10-04 01:00:10 +0100

    Fixed crash

--- src/laid_hdf5_mpi.c
+++ src/laid_hdf5_mpi.c
@@ -743,6 +743,12 @@ show_solution:
 	MPI_Win_free(&win_shared_steps);
 	MPI_Win_free(&win_shared_dset);
 
+	dataset.data = NULL;
+	dm.steps	 = NULL;
+
+	free_dataset(&dataset);
+	free_dm(&dm);
+
 	/* shut down MPI */
 	MPI_Finalize();
 
